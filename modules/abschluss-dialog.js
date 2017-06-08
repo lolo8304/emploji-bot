9ae6bfd136f68fe6222fa081d3c16aa838e6d1e8
David Ulrich <david@DavidsWebserver.local>
2017-06-08 20:01:47 +0200

    neues intro

--- modules/abschluss-dialog.js
+++ modules/abschluss-dialog.js
@@ -10,17 +10,14 @@ function AbschlussDialog(bot, builder, recognizer) {
     this.builder = builder;
     this.recognizer = recognizer;
 
-    this.bot.dialog('/Monatsabschluss', [
+    this.bot.dialog('Monatsabschluss', [
         function (session, args, next) {
             session.send("Monatsabschluss Dialog");
         },
     ])
-        .triggerAction({ matches: /Monatsabschluss/i })
         .cancelAction('/Intro', "OK Monatsabschluss abgebrochen", 
         { matches: /(start|stop|bye|goodbye|abbruch|tschüss)/i,
     onSelectAction: (session, args) => {
         session.endDialog();
-        session.beginDialog("/Intro");
-    },
-    confirmPrompt: `Are you sure you wish to cancel?`});
+    }});
 }
