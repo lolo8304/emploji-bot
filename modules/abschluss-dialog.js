1196737e096cb03988e1dd3d603dc1aebca2ca64
Bernhard BRM <b6d@gmx.ch>
2017-06-08 20:32:48 +0200

    absenzen

--- modules/abschluss-dialog.js
+++ modules/abschluss-dialog.js
@@ -12,31 +12,31 @@ function AbschlussDialog(bot, builder, recognizer) {
 
     this.bot.dialog('Monatsabschluss', [
         function (session, args, next) {
-            var user=bot.datastore.getUser();
-            var absences=bot.datastore.getAbsences();
+            var user=bot.datastore.getUser(session);
+            var absences=bot.datastore.getAbsences(session);
             var cnt=0;
             var cntOpen=0;
             for(var i in absences) {
                 cnt++;
-               if (absences[i].commit) cntOpen++;
+               if (absences[i].commit===false) cntOpen++;
             }
             if (cnt==0) {
-               session.send("Du hast keine Absenzen!");
+               session.send(user.firstname+", Du hast keine Absenzen!");
             } else {
                 if (cntOpen==0) {
-                    builder.Prompts.choice(session, "Deine "+cnt+" Absenzen sind schon bestätigt! Wenn du willst, kannst du", "Alle Absenzen anzeigen", { listStyle: builder.ListStyle.button });
+                    builder.Prompts.choice(session, user.firstname+", Deine "+cnt+" Absenzen sind schon bestätigt! Wenn du willst, kannst du", "Alle Absenzen anzeigen", { listStyle: builder.ListStyle.button });
                  } else {
-                    var text="Du hast "+cntOpen+" unbestätigte Absenzen<br>";
+                    var text=user.firstname+", Du hast "+cntOpen+" unbestätigte Absenzen<br>";
                     for(var i in absences) {
-                        if (absences[i].commit==false) {
-                            if (absences[i].days==1) {
+                        if (absences[i].commit===false) {
+                            if (absences[i].days===1) {
                               text+=absences[i].typ+" am "+absences[i].fromDate+"<br>";
                             } else {
                                text+=absences[i].days+ " Tage "+absences[i].typ+" ab "+absences[i].fromDate+"<br>";
                             }
                         }
                     }
-                    builder.Prompts.choice(session, text,  "Absenzen bestätigen", { listStyle: builder.ListStyle.button });
+                    builder.Prompts.choice(session, text,  "Monatabschluss bestätigen", { listStyle: builder.ListStyle.button });
                 }
             }
         }
